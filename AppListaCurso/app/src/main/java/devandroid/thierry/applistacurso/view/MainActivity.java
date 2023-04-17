4af3af0c2fd421615d8eb681cebab74926c0ea1a
thierry <thierryyves97@gmail.com>
2023-04-17 14:49:57 -0400

    4º criação de packages relativos ao padrão MVC

--- AppListaCurso/app/src/main/java/devandroid/thierry/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/thierry/applistacurso/view/MainActivity.java
@@ -1,8 +1,10 @@
-package devandroid.thierry.applistacurso;
+package devandroid.thierry.applistacurso.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import devandroid.thierry.applistacurso.R;
 
 public class MainActivity extends AppCompatActivity {
 
